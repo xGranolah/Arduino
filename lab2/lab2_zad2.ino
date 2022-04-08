34fc2c6c74e0638a2ffca85fc0071a3a4c39269c
xGranolah <92126483+xGranolah@users.noreply.github.com>
2022-04-08 15:28:52 +0200

    Add files via upload

---
+++ lab2/lab2_zad2.ino
@@ -0,0 +1,44 @@
+#include <TimerOne.h>
+
+int led1 = 13;
+int led2 = 12;
+int led3 = 11;
+int led4 = 10;
+
+volatile unsigned long C1 = 0;
+
+void setup() {
+
+	pinMode(led1, OUTPUT);
+	pinMode(led2, OUTPUT);
+	pinMode(led3, OUTPUT);
+	pinMode(led4, OUTPUT);
+
+	Timer1.initialize(200000);
+	Timer1.attachInterrupt(miganieLED);
+
+}
+
+void miganieLED()
+{
+	if (C1 == 5) {
+		digitalWrite(led1, LOW);
+		digitalWrite(led2, LOW);
+		digitalWrite(led3, LOW);
+		digitalWrite(led4, LOW);
+		C1 = 0;
+	}
+	else {
+		digitalWrite(led1, HIGH);
+		digitalWrite(led2, HIGH);
+		digitalWrite(led3, HIGH);
+		digitalWrite(led4, HIGH);
+		C1++;
+	}
+}
+
+void loop() 
+{
+
+
+}
