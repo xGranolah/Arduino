4a72e2e080c923230d9e8652e5d3626d05a00c03
xGranolah <92126483+xGranolah@users.noreply.github.com>
2022-04-09 22:54:01 +0200

    Update sinled.ino

--- Przykłady/sinled.ino
+++ Przykłady/sinled.ino
@@ -1,4 +1,7 @@
-
+#include <PinChangeInterruptSettings.h>
+#include <PinChangeInterruptPins.h>
+#include <PinChangeInterruptBoards.h>
+#include <PinChangeInterrupt.h>
 int led1 = 11;
 int led2 = 10;
 int led3 = 9;
@@ -6,10 +9,11 @@ int led4 = 6;
 int led5 = 5;
 int led6 = 3;
 
-int S1 = 2;
+int S1 = A4;
 int pot = A5;
 
 int C1 = 0;
+int C2 = 0;
 int y = 0;
 int y1 = 0;
 int y2 = 0;
@@ -17,12 +21,15 @@ int y3 = 0;
 int y4 = 0;
 int y5 = 0;
 
-double predkosc = 0.02;
+float predkosc = 0.02;
 int poziom = 255; //poziom światła max 255
 
 unsigned long czas = 0;
 unsigned long czas1 = 0;
+unsigned long czas2 = 0;
+unsigned long czas3 = 0;
 int T1 = 10;
+int T2 = 150;
 
 
 void setup()
@@ -35,7 +42,11 @@ void setup()
 	pinMode(led6, OUTPUT);
 	pinMode(pot, INPUT);
 
+	
+
 	pinMode(S1, INPUT);
+
+	attachPCINT(digitalPinToPCINT(A4), stan, RISING);
 
 	Serial.begin(9600);
 
@@ -54,7 +65,6 @@ void sinT()
 
 void blinksin()
 {
-	poziom = map(analogRead(pot), 0, 1023, 0, 255);
 
 	y = poziom * sin(predkosc * C1);
 	y1 = poziom * sin(predkosc * C1 - 1);
@@ -87,7 +97,7 @@ void blinksin()
 		y5 = 0;
 	}
 
-	Serial.println(poziom);
+	
 	analogWrite(led1, y);
 	analogWrite(led2, y1);
 	analogWrite(led3, y2);
@@ -96,9 +106,54 @@ void blinksin()
 	analogWrite(led6, y5);
 }
 
+void stan()
+{
+	if (digitalRead(S1) == HIGH)
+	{
+		C2++;
+	}
+}
+
+void predkoscled()
+{
+	if (C2 >= 3)
+	{
+		C2 = 0;
+	}
+	if (C2 == 1)
+	{
+		poziom = map(analogRead(pot), 0, 1023, 0, 255);
+	}
+	if (czas - czas3 >= 150)
+	{
+		czas3 = czas;
+		if (C2 == 0)
+		{
+			predkosc = (analogRead(pot) / 1023.000) * 0.500;
+		}
+	}
+	
+}
+
 
 void loop()
 {
+	if (czas - czas2 >= 1000)
+	{
+		czas2 = czas;
+		Serial.print("Poziom swiatla: ");
+		Serial.print(poziom);
+		Serial.println();
+		Serial.print("Predkosc: ");
+		Serial.print(predkosc);
+		Serial.println();
+		Serial.print("Kanal: ");
+		Serial.print(C2);
+		Serial.println();
+	}
+
+
 	sinT();
+	predkoscled();
 	blinksin();
 }
